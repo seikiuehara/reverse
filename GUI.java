08bcaaccffefd3267fa63ba63bc49c985ee164a2
seikiuehara <seiki.tennisgiants06@gmail.com>
2025-05-06 12:09:56 +0900

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,1 +1,14 @@
+import javax.swing.*;
 
+public class GUI {
+    public static void main(String[] args) {
+        // Othelloゲームロジックの初期化
+        Othello game = new Othello();
+
+        // GUIクライアントの初期化
+        SwingUtilities.invokeLater(() -> {
+            Client client = new Client(game);
+            client.setVisible(true);
+        });
+    }
+}
